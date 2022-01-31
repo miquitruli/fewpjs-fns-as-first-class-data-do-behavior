5098a0d28d36395380f9332efe866de95c61ee0f
Micaella Colombo <micaelc@g.clemson.edu>
2022-01-30 19:40:04 -0500

    commit tree 5518c3eaea71a5e0b12dda3a9c0402bd632f4065

--- index.js
+++ index.js
@@ -10,4 +10,28 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time){
+  const timeString = parseInt((time.split(":")),10);
+  /* The parseInt method parses a value as a string and returns the first integer.
+  A radix parameter specifies the number system to use:
+  2 = binary, 8 = octal, 10 = decimal, 16 = hexadecimal.
+  parseInt(string, radix) */
+
+  if (timeString < 12) {
+    return displayMessage("Good Morning");
+  }
+  else if (timeString >= 12 && timeString <= 17) {
+    return displayMessage("Good Afternoon");
+  }
+  else {
+    return displayMessage("Good Evening");
+  }
+}
 /* Write your implementation of displayMessage() */
+
+
+function displayMessage(message) {
+  const element = document.getElementById("greeting");
+  element.innerText = message;
+  return message;
+}
